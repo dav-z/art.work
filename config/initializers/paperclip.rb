d9cbda83173b28b201d7a948a1ee9f8b9e707fe8
David Zheng <davidzheng@knights.ucf.edu>
2017-08-22 12:46:22 -0400

    pics?

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,2 @@
 Paperclip::Attachment.default_options[:use_timestamp] = false
-Paperclip::Attachment.default_options[:s3_host_name] = 'https://s3-us-east-2.amazonaws.com'
+Paperclip::Attachment.default_options[:s3_host_name] = 'http://s3-us-east-2.amazonaws.com'
